3a8c25e4f18c963c6694eceb3c83d52fa5d6b429
myrean <dhughes1@uci.edu>
2018-11-11 21:11:41 -0800

    main ui 1st attempt

---
+++ assets/javascript/spaceballs.js
@@ -0,0 +1,23 @@
+//Global Variables
+//==================================
+
+var characters = ["dot", "helmet", "barf", "lonestar", "vespa"];
+var messages = ["No Sir! I did not see you playing with your dolls again!", "We Brake for No One",];
+
+
+
+
+
+
+//Functions
+//===================================
+
+
+
+
+
+
+
+
+//Main
+//=====================================
