a55edb5fa2b0443d2962624ecfe5148a40c188a5
myrean <dhughes1@uci.edu>
2018-11-18 00:19:04 -0800

    updated hp and names created player area

--- assets/javascript/spaceballs.js
+++ assets/javascript/spaceballs.js
@@ -2,10 +2,15 @@
 //==================================
 
 
-var dollsImg;
 var messages = ["No Sir! I did not see you playing with your dolls again!", "We Brake for No One", "Comb the desert!"];
-
-
+var player = [
+    {
+        name: "Player",
+        score: 0,
+        kills: 0,
+        wins: 0
+    }
+];
 var spaceballs = [
     {
         name: "Barf",
@@ -32,7 +37,7 @@ var spaceballs = [
         cap: 2,
         src: "assets/images/dot.png",
         width: 50,
-        height: 60,
+        height: 60
     },
     {
         name: "Lonestar",
@@ -55,52 +60,63 @@ var spaceballs = [
 ];
 
 
-
 //Functions
 //===================================
 function load_dolls() {
-
+    
     spaceballs.forEach(function (elem, i) {
         console.log(elem);
+        
+        //Loads the Dolls images
         dollsImg = $("<img>");
-        dollsImg.addClass("img-fluid clickableimage");
+        dollsImg.addClass("img-fluid clickable");
         dollsImg.attr("src", elem.src);
         dollsImg.attr("width", elem.width);
         dollsImg.attr("height", elem.height);
-
         $("#img" + i).append(dollsImg);
 
-        // $("<div class='hp'> Hit Points>").append(dollsImg);
+
+        // Loads the Dolls HP
+        dollsHP = $("<div>");
+        dollsHP.text(elem.hp);
+        dollsHP.attr("hp", elem.hp);
+        $("#hp" + i).append(dollsHP);
+
+        // Loads the Dolls Names
+        dollsName = $("<div>");
+        dollsName.text(elem.name);
+        dollsName.attr("name", elem.name);
+        $("#name" + i).append(dollsName);
+
+
+
+        $('.clickable').click(function () {
+            $(this).prependTo('#player');
+
+    
 
         
-        // dollsImg.append("<div class='hp'> Hit Points");
-        // dollsHP = $("<div>");
-        // dollsHP.text("HP");
-        // dollsHP.attr("hp", elem.hp);
-
-
-
-    });
-
-
-
-    $('.clickableimage').click(function () {
-        $(this).attr("hp", elem.hp);
     })
 
+       
+    });
+        
+        console.log(spaceballs);
+   
+
 };
-
-
-
-
 
 
 function startGame() {
+    var dollsName;
+    var dollsImg;
+    var dollsHP;
+    var dollsName;
+    var player;
+
     showDolls();
 
 };
-
-
 
 
 function moveDolls() {
@@ -111,8 +127,6 @@ function showDolls() {
 
     load_dolls();
 
-
-
 };
 
 function reset() {
@@ -122,9 +136,6 @@ function reset() {
 //Main
 //=====================================
 
-
-
 $('#playbtn').click(startGame);
 $('#restart').click(reset);
-
 
