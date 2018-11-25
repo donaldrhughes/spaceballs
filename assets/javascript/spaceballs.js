e17d9d294f138c01351d4b153626db4fe2437372
myrean <dhughes1@uci.edu>
2018-11-24 16:33:45 -0800

    many updates -- ui, functionality -- resets and Dom

--- assets/javascript/spaceballs.js
+++ assets/javascript/spaceballs.js
@@ -2,17 +2,28 @@
 //==================================
 
 
-var messages = ["No Sir! I did not see you playing with your dolls again!", "We Brake for No One", "Comb the desert!"];
+var messages = ["There's only one man who would dare give me the raspberry: Lonestarr!","What's the matter, Colonel Sandurz? Chicken?","It's the radar. It appears to be... Jammed.","No Sir! I did not see you playing with your dolls again!", "We Brake for No One.", "Comb the desert!", "Ludicrous Speed", "What?! You went over my helmet?", "You have the ring, and I see your Schwartz is as big as mine. Now let’s see how well you handle it.", "I’ve lost the bleeps, I’ve lost the sweeps, and I’ve lost the creeps. That's not all he's lost!", "Who knows? God willing, we’ll all meet again in Spaceballs 2: The Search for More Money.", "My Brains are going into my feet!", "It's Spaceball One -- They've gone to plaid...", "That's the stupidest combination I've ever heard of in my life! That's the kinda thing an idiot would have on his luggage!"];
+var message = messages[Math.floor(Math.random()*messages.length)];
 var player = [
     {
         name: "Player",
+        hp: 0,
+        ap: 0,
+        cap: 0,
         score: 0,
         kills: 0,
-        wins: 0
+        wins: 0,
+        width: 0,
+        height: 0,
+        src: ""
     }
 ];
+
 var spaceballs = [
     {
+        doll: "barf",
+        dollnum: "",
+        isPlayer: false,
         name: "Barf",
         hp: 75,
         ap: 2,
@@ -22,6 +33,9 @@ var spaceballs = [
         height: 50
     },
     {
+        doll: "helmet",
+        dollnum: "",
+        isPlayer: false,
         name: "Dark Helmet",
         hp: 100,
         ap: 6,
@@ -31,6 +45,9 @@ var spaceballs = [
         height: 50
     },
     {
+        doll: "dot",
+        dollnum: "",
+        isPlayer: false,
         name: "Dot Matrix",
         hp: 120,
         ap: 5,
@@ -40,7 +57,10 @@ var spaceballs = [
         height: 60
     },
     {
-        name: "Lonestar",
+        doll: "Lonestarr",
+        dollnum: "",
+        isPlayer: false,
+        name: "Lonestarr",
         hp: 80,
         ap: 5,
         cap: 5,
@@ -49,6 +69,9 @@ var spaceballs = [
         height: 50
     },
     {
+        doll: "vespa",
+        dollnum: "",
+        isPlayer: false,
         name: "Princess Vespa",
         hp: 85,
         ap: 3,
@@ -63,13 +86,17 @@ var spaceballs = [
 //Functions
 //===================================
 function load_dolls() {
-    
+
     spaceballs.forEach(function (elem, i) {
+
         console.log(elem);
-        
+        var dollNum = ('doll' + i);
+        spaceballs[i].dollnum = i;
+        console.log(spaceballs[i].dollnum);
+
         //Loads the Dolls images
         dollsImg = $("<img>");
-        dollsImg.addClass("img-fluid clickable");
+        dollsImg.addClass("img-fluid border");
         dollsImg.attr("src", elem.src);
         dollsImg.attr("width", elem.width);
         dollsImg.attr("height", elem.height);
@@ -90,32 +117,48 @@ function load_dolls() {
 
 
 
-        $('.clickable').click(function () {
-            $(this).prependTo('#player');
+        $("#doll" + i).click(
+            function () {
+                $("#doll" + i).prependTo('#player');
+                spaceballs[i].isPlayer == true;
 
-    
+                player[0].doll = spaceballs[i].doll;
+                player[0].hp = spaceballs[i].hp;
+                player[0].ap = spaceballs[i].ap;
+                player[0].cap = spaceballs[i].cap;
+                player[0].src = spaceballs[i].src;
+                player[0].width = spaceballs[i].width;
+                player[0].height = spaceballs[i].height;
 
-        
-    })
 
-       
+
+                $('#atk-btn').show();
+                message = "<<< Click Attack to Fight >>>";
+    $('#gameMsg').text("message");
+            })
+
+
     });
-        
-        console.log(spaceballs);
-   
+
+    console.log(spaceballs);
+
 
 };
 
 
 function startGame() {
-    var dollsName;
-    var dollsImg;
-    var dollsHP;
-    var dollsName;
-    var player;
+    message = "<<< Click a Doll to Play >>>";
+    $('#gameMsg').text(message);
+    $('#playbtn').hide();
+    $('#resetbtn').show();
+    $('#resetbtn').click(reset);
+    var dollsName = "";
+    var dollsImg = "";
+    var dollsHP = 0;
 
-    showDolls();
-
+    load_dolls();
+    $('#atk-btn').show();
+    $('#atk-btn').click(battle);
 };
 
 
@@ -123,19 +166,38 @@ function moveDolls() {
 
 };
 
-function showDolls() {
 
-    load_dolls();
-
-};
 
 function reset() {
+    var dollsName = "";
+    var dollsImg = "";
+    var dollsHP = 0;
+    for (i = 0; i < spaceballs.length; i++) {
+        $('#img' + spaceballs[i].dollnum).empty();
+        $('#hp' + spaceballs[i].dollnum).empty();
+        $('#name' + spaceballs[i].dollnum).empty();
+        $('#player').empty();
+        message = messages[Math.floor(Math.random()*messages.length)];
+        $('#gameMsg').text(message);
+        $('#atk-btn').hide();
+    };
 
+    $('#playbtn').show();
+
+    $('#resetbtn').hide();
+
+}
+
+function battle() {
+    console.log(player);
+    console.log("clicked");
 }
 
 //Main
 //=====================================
 
 $('#playbtn').click(startGame);
-$('#restart').click(reset);
-
+$('#resetbtn').hide();
+$('#atk-btn').hide();
+$('#gameMsg').text(message)
+ 
